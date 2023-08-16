31416baf54135eba53976e74b6d0c7ffade4450d
sxxhlxxdb <2297251502@qq.com>
2023-08-16 23:08:18 +0800

    样式布局优化

--- packages/ui/src/views/DependencyGraph/config/graphConfig.ts
+++ packages/ui/src/views/DependencyGraph/config/graphConfig.ts
@@ -1,43 +1,78 @@
-import {GraphOptions} from "@antv/g6-core/lib/types";
+import { GraphOptions } from "@antv/g6-core/lib/types";
+import G6 from "@antv/g6";
+
+// 提示框
+const tooltip = new G6.Tooltip({
+  offsetX: 10,
+  offsetY: 10,
+  // the types of items that allow the tooltip show up
+  // 允许出现 tooltip 的 item 类型
+  itemTypes: ['node', 'edge'],
+  // custom the tooltip's content
+  // 自定义 tooltip 内容
+  getContent: (e) => {
+    const outDiv = document.createElement('div');
+    outDiv.style.width = 'fit-content';
+    outDiv.style.padding = '0px 20px 10px 0px';
+    if(e.target.get('name') === 'edge-shape'){
+      outDiv.innerHTML = `
+      <h4 style="margin-left:20px">边信息</h4>
+      <ul>
+        <li>依赖类型: ${e.item.getModel().info}</li>
+      </ul>
+      `;
+    }else{
+      {
+        outDiv.innerHTML = `
+        <h4 style="margin-left:20px">节点信息</h4>
+        <ul>
+          <li>名称: ${e.item.getModel().name}</li>
+        </ul>
+        <ul>
+          <li>版本: ${e.item.getModel().version}</li>
+        </ul>
+        <ul>
+          <li>路径: ${e.item.getModel().path}</li>
+        </ul>
+        `;
+      }
+    }
+    return outDiv;
+  },
+});
 
 const defaultG6Graph: GraphOptions = {
-    container: "graph-container",
-    width: 1920,
-    height: 1080,
-    fitView: true,
-    defaultNode: {
-        type: 'circle',
-        size: 100,
-        style: {
-            fill: '#ffffff',
-            stroke: '#f1f1f1f1'
-        },
-        labelCfg: {
-            style: {
-                fill: '#000', // 节点标签文字颜色
-            },
-        }
-    },
-    defaultEdge: {
-        color: '#000',
-        style: {
-            endArrow: true,
-
-        }
-    },
-    layout: {
-        type: 'force2',
-        preventOverlap: true,
-        nodeSize: 200,
-        edgeStrength: 500,
-        nodeStrength: 1500,
-        nodeSpacing: 20,
-        animate: true,
-        maxSpeed: 2000
-    },
-    modes: {
-        default: ['drag-canvas', 'zoom-canvas', 'drag-node', 'activate-relations']
+  container: "graph-container",
+  width: 1920,
+  height: 1080,
+  fitView: true,
+  defaultNode: {
+    type: 'rect',
+    size: [200, 50],
+  },
+  defaultEdge: {
+    type: 'polyline',
+    size: 3,
+    color: '#D1C2EF',
+    style: {
+      endArrow: {
+        path: 'M 0,0 L 8,4 L 8,-4 Z',
+        fill: '#e2e2e2',
+      },
+      radius: 20,
     }
+  },
+  layout: {
+    type: 'dagre',
+    rankdir: 'LR',
+    controlPoints: true,
+    nodesepFunc: () => 1,
+    ranksepFunc: () => 30,
+  },
+  modes: {
+    default: ['drag-canvas', 'zoom-canvas', 'drag-node', 'activate-relations']
+  },
+  plugins: [tooltip],
 }
 
-export {defaultG6Graph}
+export { defaultG6Graph }
--- packages/ui/src/views/DependencyGraph/index.vue
+++ packages/ui/src/views/DependencyGraph/index.vue
@@ -1,48 +1,106 @@
 <script setup lang="ts">
-import {GraphData} from "@antv/g6";
-import {onMounted, ref, watch, watchEffect} from "vue";
-import {defaultG6Graph} from "./config/graphConfig";
-import {GraphLayoutPredict} from "@antv/vis-predict-engine";
-import G6 from '@antv/g6'
-import {getOriginalObjectOfProxy} from "../../utils/getOriginalObjectOfProxy.ts";
+import { GraphData } from "@antv/g6";
+import { onMounted, ref, watch, watchEffect } from "vue";
+import { defaultG6Graph } from "./config/graphConfig";
+import { GraphLayoutPredict } from "@antv/vis-predict-engine";
+import G6 from "@antv/g6";
+import { getOriginalObjectOfProxy } from "../../utils/getOriginalObjectOfProxy.ts";
 
 const props = defineProps<{
-  height: number,
-  width: number,
-  data: GraphData
+    height: number;
+    width: number;
+    data: GraphData;
 }>();
 
 const emit = defineEmits<{
-  nodeClick: [id: number]
+    nodeClick: [id: number];
 }>();
 
+const colors = [
+    "#BDD2FD",
+    "#BDEFDB",
+    "#C2C8D5",
+    "#FBE5A2",
+    "#F6C3B7",
+    "#B6E3F5",
+    "#D3C6EA",
+    "#FFD8B8",
+    "#AAD8D8",
+    "#FFD6E7",
+];
+const strokes = [
+    "#5B8FF9",
+    "#5AD8A6",
+    "#5D7092",
+    "#F6BD16",
+    "#E8684A",
+    "#6DC8EC",
+    "#9270CA",
+    "#FF9D4D",
+    "#269A99",
+    "#FF99C3",
+];
 
-onMounted( () => {
-  const graph = new G6.Graph(defaultG6Graph);
-  // // AI预测图最适合的布局
-  // const {predictLayout, confidence} = await GraphLayoutPredict.predict(getOriginalObjectOfProxy(props.data));
-  // defaultG6Graph.layout.type = predictLayout;
-  graph.data(getOriginalObjectOfProxy(props.data));
+// 自定义节点
+const nodes = props.data.nodes;
+const depthMap = new Map();
+let depthId = 0;
+nodes.forEach((element) => {
+    // cluster
+    if (element.cluster && depthMap.get(element.cluster) === undefined) {
+        depthMap.set(element.cluster, depthId);
+        depthId++;
+    }
+    const cid = depthMap.get(element.cluster);
+    if (!element.style) {
+        element.style = {};
+    }
+    element.style.fill = colors[cid % colors.length];
+    element.style.stroke = strokes[cid % strokes.length];
+});
 
+// 自定义边
+const edges = props.data.edges;
+const edgesMap = new Map();
+let edgesId = 0;
+edges.forEach((element) => {
+    if (element.info && edgesMap.get(element.info) === undefined) {
+        edgesMap.set(element.info, edgesId);
+        edgesId++;
+    }
+    const cid = edgesMap.get(element.info);
+    if (!element.style) {
+        element.style = {};
+    }
+    element.color = colors[cid % colors.length];
+});
 
-  // 监听数据变化， 自动重新渲染图
-  watch(() => props.data, () => {
-    graph.changeData(getOriginalObjectOfProxy(props.data));
-  });
+onMounted(() => {
+    const graph = new G6.Graph(defaultG6Graph);
+    // // AI预测图最适合的布局
+    // const {predictLayout, confidence} = await GraphLayoutPredict.predict(getOriginalObjectOfProxy(props.data));
+    // defaultG6Graph.layout.type = predictLayout;
+    graph.data(getOriginalObjectOfProxy(props.data));
 
-  // 顶点点击事件
-  graph.on('node:click', (e) => {
-    emit('nodeClick', e.item.getID());
-  })
+    // 监听数据变化， 自动重新渲染图
+    watch(
+        () => props.data,
+        () => {
+            graph.changeData(getOriginalObjectOfProxy(props.data));
+        }
+    );
 
-  graph.render();
-})
+    // 顶点点击事件
+    graph.on("node:click", (e) => {
+        emit("nodeClick", e.item.getID());
+    });
+
+    graph.render();
+});
 </script>
 
 <template>
-  <div class="graph-container"></div>
+    <div class="graph-container"></div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
