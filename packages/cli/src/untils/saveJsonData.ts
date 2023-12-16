2009dd5bf5b0207ced15a8ac28fe64eb3f020738
songyiping <songyiping03@baidu.com>
2023-12-16 17:09:59 +0800

    fix(cli): 修复对npa-core中类型的错误访问

--- packages/cli/src/untils/saveJsonData.ts
+++ packages/cli/src/untils/saveJsonData.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import {JsonData} from "npa-core/dist/types/JsonData";
+import {JsonData} from "npa-core";
 
 function saveJsonData(pth: string, data: JsonData) {
     fs.writeFileSync(pth, JSON.stringify(data, null, 2));
