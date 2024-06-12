9ee591dd229231e040317d977c706d97bc542a69
trudbot <SYP2185759039@163.com>
2024-06-12 21:50:13 +0800

    chore(cli): 优化cli开发模式打包策略

--- packages/cli/rollup.config.dev.js
+++ packages/cli/rollup.config.dev.js
@@ -2,6 +2,7 @@ import configs from './rollup.config.default.js'
 import replace from '@rollup/plugin-replace';
 
 export default configs.map(config => {
+    config.external = ['chalk', 'figlet', 'npa-core', 'express', 'open', 'express', 'commander', 'glob', 'hirestime', 'portfinder'];
     config.plugins.push(replace({
         values: {
             'process.env.NPA_ENV': JSON.stringify('development')
